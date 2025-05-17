e1ce8dd280f13da81f0d45073d2aeee4b872ea8d
U82146510 <U82146510@protonmail.com>
2025-05-17 16:49:06 +0100

    updates on authorization

--- middleware/authorization.ts
+++ middleware/authorization.ts
@@ -1,13 +1,23 @@
+import { error } from "console";
 import { type Request,type Response,type NextFunction } from "express";
 
-export const authoriz =(role:'admin'|'moderator'|'user')=> (req:Request,res:Response,next:NextFunction)=>{
+
+const rolesHierarchy = ['user', 'moderator', 'admin'] as const;
+
+export const authoriz =(requiredRole:'admin'|'moderator'|'user')=> (req:Request,res:Response,next:NextFunction)=>{
     try {
-        if(role!==req.user?.role){
-            res.status(403).json({error:'Forbidden',message:'You have insufficient permission'});
+        const userRole = req.user?.role;
+        if(!userRole){
+            res.status(401).json({error:'Unauthorized',message:'No user role found'});
             return;
         }
-        next()
+        if(rolesHierarchy.indexOf(userRole)<rolesHierarchy.indexOf(requiredRole)){
+            res.status(403).json({error:'Forbidden',message:'Insufficient permission'});
+            return;
+        }
+        next();
     } catch (error) {
         console.error(error)
+        next(error);
     }
 };
